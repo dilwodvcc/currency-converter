fa6f1192bdd35cf1ed63a5e0fcd8fae03ea2e65e
dilwodvc <abdullokh207@example.com>
2024-11-13 13:51:00 +0500

    conventatsiya qilishini mexanizatwtirildi

--- Currency.php
+++ Currency.php
@@ -1,6 +1,7 @@
 <?php 
 class Currency{
  const CURRENCY_API_URL = "https://cbu.uz/uz/arkhiv-kursov-valyut/json/";
+ public $currencies;
  public function __construct(){
 
     $ch = curl_init();
@@ -9,8 +10,31 @@ class Currency{
 
     $output = curl_exec($ch);
     curl_close($ch);
-    $decoded = json_decode($output);
-    echo ($decoded[0]->Code);
+    $this->currencies = json_decode($output);
     }
+public function getCurrencies():array{
+    $separated_date = [];
+    $currensies_info = $this->currencies;
+    foreach($currensies_info as $currency){
+        $separated_date[$currency->Ccy] = $currency->Rate;}
+        return $separated_date;
 }
+public function exchange($value, $from, $to) {
+    $currencies = $this->getCurrencies();
+
+    if (!isset($currencies[$from]) || !isset($currencies[$to])) {
+        throw new Exception("Valyuta topilmadi.");
+    }
+
+    $fromRate = $currencies[$from];
+    $toRate = $currencies[$to];
+
+
+    $convertedValue = ($value / $fromRate) * $toRate;
+    return round($convertedValue, 2) . ' ' . $to;
+}
+
+}
+
+$currency = new Currency() ;
 ?>
